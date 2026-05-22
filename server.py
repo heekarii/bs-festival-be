ad3ee48feb6c63558aa74838f9d1bb0c97dbfc60
Heeseok Cho <hantecude123@gmail.com>
2026-05-22 18:40:56 +0900

    server.py 업데이트

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_origins=["http://localhost:3000",
                    "https://localhost:3000",
                    "https://festival-jrgq6rxju-choheeseoks-projects.vercel.app",
-                   "bs-festival-fe.vercel.app",
+                   "https://bs-festival-fe.vercel.app",
                    "https://festival.heekari.com"
                    ],
     allow_methods=["*"],
